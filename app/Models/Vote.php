9ca6429a82d64e1fad2ac0c94e65ca58aaeda7c6
KianHmz <hmz00abd@gmail.com>
2025-07-12 22:56:21 +0330

    fix bugs & refactor

--- app/Models/Vote.php
+++ app/Models/Vote.php
@@ -3,10 +3,14 @@
 namespace App\Models;
 
 // use MongoDB\Laravel\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
 
 class Vote extends Model
 {
+    use HasFactory, Notifiable;
+
     // protected $connection = 'mongodb';
 
     protected $fillable = [
