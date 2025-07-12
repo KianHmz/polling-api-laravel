cfa0d63f203cda06a2f77d1cb314bde57404e075
KianHmz <hmz00abd@gmail.com>
2025-07-12 14:45:25 +0330

    define guest middleware

--- routes/api.php
+++ routes/api.php
@@ -8,5 +8,7 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::get('/user', [AuthController::class, 'user']);
 });
 
-Route::post('/register', [AuthController::class, 'register']);
-Route::post('/login', [AuthController::class, 'login']);
+Route::middleware('auth:guest')->group(function () {
+    Route::post('/register', [AuthController::class, 'register']);
+    Route::post('/login', [AuthController::class, 'login']);
+});
