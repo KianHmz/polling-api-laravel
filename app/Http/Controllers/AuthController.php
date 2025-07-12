55bb881c2ffd925c314dd341127693e4283ff799
KianHmz <hmz00abd@gmail.com>
2025-07-12 15:37:15 +0330

    configure login action

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
 
 class AuthController extends Controller
 {
@@ -12,7 +14,7 @@ class AuthController extends Controller
         $credentials = $request->validate([
             'name' => 'required|string|min:3|max:255',
             'email' => 'required|email|unique:users,email',
-            'password' => 'required|max:255|confirmed',
+            'password' => 'required|string|max:255|confirmed',
         ]);
 
         $user = User::create($credentials);
@@ -27,8 +29,28 @@ class AuthController extends Controller
 
     }
 
-    public function login()
+    public function login(Request $request)
     {
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required|string|max:255'
+        ]);
+
+        $user = User::where('email', $request->email)->first();
+
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            throw ValidationException::withMessages([
+                'email' => 'The provided credentials do not match our records!'
+            ]);
+        }
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            'message' => 'Logged-in successfully!',
+            'access_token' => $token,
+            'token_type' => 'Bearer'
+        ]);
     }
 
     public function logout()
