b4b60b9e282f943be51728ad8664a02cea10b7c2
KianHmz <hmz00abd@gmail.com>
2025-07-12 22:47:59 +0330

    handle exceptions

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,114 +3,72 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use Illuminate\Auth\AuthenticationException;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\ValidationException;
+use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 
 class AuthController extends Controller
 {
     public function register(Request $request)
     {
-        try {
-            $request->validate([
-                'name' => 'required|string|min:3|max:255',
-                'email' => 'required|email|unique:users,email',
-                'password' => 'required|string|max:255|confirmed',
-            ]);
+        $request->validate([
+            'name' => 'required|string|min:3|max:255',
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required|string|max:255|confirmed',
+        ]);
 
-            $user = User::create([
-                'name' => $request->name,
-                'email' => $request->email,
-                'password' => Hash::make($request->password),
-            ]);
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
 
-            $token = $user->createToken('auth_token')->plainTextToken;
+        $token = $user->createToken('auth_token')->plainTextToken;
 
-            return response()->json([
-                'message' => 'Registered successfully!',
-                'access_token' => $token,
-                'token_type' => 'Bearer'
-            ], 201);
-
-        } catch (ValidationException $e) {
-            return response()->json([
-                'message' => 'Validation error!',
-                'errors' => $e->errors(),
-            ], 422);
-
-        } catch (\Throwable $th) {
-            return response()->json([
-                'message' => 'Server error!',
-                'error' => $th->getMessage(),
-            ], 500);
-        }
+        return response()->json([
+            'message' => 'Registered successfully!',
+            'access_token' => $token,
+            'token_type' => 'Bearer'
+        ], 201);
     }
 
     public function login(Request $request)
     {
-        try {
-            $request->validate([
-                'email' => 'required|email',
-                'password' => 'required|string|max:255'
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required|string|max:255'
+        ]);
+
+        $user = User::where('email', $request->email)->first();
+
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            throw ValidationException::withMessages([
+                'email' => ['The provided credentials do not match our records!'],
             ]);
-
-            $user = User::where('email', $request->email)->first();
-
-            if (!$user || !Hash::check($request->password, $user->password)) {
-                throw ValidationException::withMessages([
-                    'email' => ['The provided credentials do not match our records!'],
-                ]);
-            }
-
-            $token = $user->createToken('auth_token')->plainTextToken;
-
-            return response()->json([
-                'message' => 'Logged-in successfully!',
-                'access_token' => $token,
-                'token_type' => 'Bearer'
-            ], 200);
-
-        } catch (ValidationException $e) {
-            return response()->json([
-                'message' => 'Validation error!',
-                'errors' => $e->errors(),
-            ], 422);
-
-        } catch (\Throwable $th) {
-            return response()->json([
-                'message' => 'Server error!',
-                'error' => $th->getMessage(),
-            ], 500);
         }
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            'message' => 'Logged-in successfully!',
+            'access_token' => $token,
+            'token_type' => 'Bearer'
+        ], 200);
     }
 
     public function logout(Request $request)
     {
-        try {
-            $request->user()->currentAccessToken()->delete();
+        $request->user()->currentAccessToken()->delete();
 
-            return response()->json([
-                'message' => 'Logged-out successfully!',
-            ]);
-
-        } catch (\Throwable $th) {
-            return response()->json([
-                'message' => 'Server error!',
-                'error' => $th->getMessage(),
-            ], 500);
-        }
+        return response()->json([
+            'message' => 'Logged-out successfully!',
+        ]);
     }
 
     public function user(Request $request)
     {
-        try {
-            return response()->json($request->user());
-
-        } catch (\Throwable $th) {
-            return response()->json([
-                'message' => 'Server error!',
-                'error' => $th->getMessage(),
-            ], 500);
-        }
+        return response()->json($request->user());
     }
 }
