6bf508afd84a436788aa3921ca74ee3325a77925
KianHmz <hmz00abd@gmail.com>
2025-07-12 15:30:40 +0330

    configure register action

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,12 +2,29 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    public function register()
+    public function register(Request $request)
     {
+        $credentials = $request->validate([
+            'name' => 'required|string|min:3|max:255',
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required|max:255|confirmed',
+        ]);
+
+        $user = User::create($credentials);
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            'message' => 'Registered successfully!',
+            'access_token' => $token,
+            'token_type' => 'Bearer'
+        ]);
+
     }
 
     public function login()
